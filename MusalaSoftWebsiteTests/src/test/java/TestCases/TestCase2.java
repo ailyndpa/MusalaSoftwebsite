a372cb52fa5c2bff73e55f15720dc84a279c5997
Ailyn del Pino Acosta <ailyndpa@gmail.com>
2022-10-12 00:39:15 -0300

    adding test case 2

--- MusalaSoftWebsiteTests/src/test/java/TestCases/TestCase2.java
+++ MusalaSoftWebsiteTests/src/test/java/TestCases/TestCase2.java
@@ -1,7 +1,10 @@
 package TestCases;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
@@ -30,8 +33,9 @@ public class TestCase2 {
 	}
 
 	@Test
-	public void company() {
-		
+	public void company() throws InterruptedException {
+		driver.findElement(By.xpath("//div[@id='menu']/ul[@id='menu-main-nav-1']/li[1]/a")).click();
+		System.out.println(driver.getCurrentUrl());		
 	}
 
 }
